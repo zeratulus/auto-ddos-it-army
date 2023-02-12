fbd066751d8e490122c78a2e3ec6a045e2d74b0b
Serhii Herenko <ailus665@gmail.com>
2023-02-13 01:27:55 +0200

    Rollback of check internet connection.

--- auto-ddos.sh
+++ auto-ddos.sh
@@ -10,19 +10,19 @@ checkInetConnection() {
   return 0;
 }
 
-# Wait for internet connection
-#isInternet=false
-#while [ $isInternet == false ]; do
-#  isInternet=checkInetConnection;
-#  sleep 5
-#done
+#sleep 360;
 
-sleep 360;
+# Wait for internet connection
+isInternet=false
+while [ $isInternet == false ]; do
+  isInternet=checkInetConnection;
+  sleep 5
+done
 
 if [ $isInternet ]; then
   echo "Connected to internet";
   chmod +x ~/db1000n/db1000n
-  protonvpn-cli c -f          #Connect to fastest server to your location
-  #protonvpn-cli c --cc JP    #Connect to fastest server in country JP - Japan
+#  protonvpn-cli c -f          #Connect to fastest server to your location
+  protonvpn-cli c --cc JP    #Connect to fastest server in country JP - Japan
   ~/db1000n/db1000n --user-id=302116940
 fi
