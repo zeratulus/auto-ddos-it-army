36419ad1a35528e0a2dbc8e71e35557c8f15a491
Serhii Herenko <ailus665@gmail.com>
2023-06-05 14:56:59 +0300

    Added max port range

--- install-mhddos.sh
+++ install-mhddos.sh
@@ -3,3 +3,5 @@
 sudo apt update -y && sudo apt install --upgrade wget screen -y
 
 cd ~ && wget https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux && chmod +x mhddos_proxy_linux
+
+sysctl -w net.ipv4.ip_local_port_range="16384 65535"
